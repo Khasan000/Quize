f3ce981df0991db124dc80fa54c7f076e43b168e
Khasan000 <zkurycin@gmail.com>
2023-06-23 13:04:31 +0300

    Rework Manager

--- Classes/FileWriter.cs
+++ Classes/FileWriter.cs
@@ -4,25 +4,40 @@ namespace ConsoleApp;
 
 public class FileWriter
 {
-    public static void CreateFile(string login, string password)
+    public static void CreateFile(string login, string password, DateTime dateTime)
     {
         Console.Clear();
         using (StreamWriter writer = new StreamWriter(login + ".txt"))
         {
             writer.WriteLine(password);
+            
+            writer.Write(dateTime.Date.Day+".");
+            writer.Write(dateTime.Date.Month+".");
+            writer.Write(dateTime.Date.Year);
+            
             writer.Close();
         }
     }
     public static void ChangePassword(string login, string password, string newPassword)
     {
         Console.Clear();
-       
-        if (!File.Exists(login + ".txt"))
+
+        if (!CheckPassword(login, password))
         {
-            Console.WriteLine($"Account {login} does not exist");
+            Console.WriteLine("Incorrect password!");
             return;
         }
         
+        string[] lines = File.ReadAllLines(login + ".txt");//
+        lines[0] = newPassword;
+
+        File.WriteAllLines(login+".txt",lines);
+    }
+
+    public static bool CheckPassword(string login,string password)
+    {
+        Console.Clear();
+
         using (StreamReader reader = new StreamReader(login + ".txt"))
         {
             string passwordInFile = reader.ReadLine();
@@ -30,18 +45,14 @@ public class FileWriter
 
             if (passwordInFile != password)
             {
-                Console.WriteLine("Incorrect password!");
-                return;
+                return false;
             }
-        }
-        
-        string[] lines = File.ReadAllLines(login + ".txt");//
 
-        lines[0] = newPassword;
-            
-            
-        File.WriteAllLines(login+".txt",lines);
+            return true;
+        }
     }
+    
+    
     
 
     
